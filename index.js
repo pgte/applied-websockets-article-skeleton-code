0a9fd15dff05e9d0f1107e9286c024c55e7c44fd
Pedro Teixeira <pedro.teixeira@gmail.com>
2013-07-12 12:10:29 +0100

    skeleton

---
+++ index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+var store = require('./store');
+var server = require('http').createServer();
+
+var port = parseInt(process.argv[2]) || 8000;
+server.listen(port);
+
+server.once('listening', function() {
+  console.log('Server is listening on port %d', port);
+});
+
+/// Static file serving
+
+var static = require('node-static');
+var file = new (static.Server)(__dirname + '/browser');
+server.on('request', function(req, res) {
+
+  file.serve(req, res, function(err) {
+    if (err) {
+      if (err.status == 404 && ~req.headers.accept.split(',').indexOf('text/html')) {
+        file.serveFile('/index.html', 200, {}, req, res);
+      } else {
+        console.error('error processing request %s %s:', req.method, req.url, err);
+        res.statusCode = err.status;
+        res.setHeader('content-type', 'application/json');
+        res.end(JSON.stringify({error: { message: err.message}}));
+      }
+    }
+  });
+});
